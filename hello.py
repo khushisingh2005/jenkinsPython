52348f037b34f8ffeeb1fb0681f9f77e47b3f247
khushisingh2005 <khushisingh2822005@gmail.com>
2024-10-14 20:26:48 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This is a simple Python program
+
+# Print a welcome message
+print("Hello, World!")
+
+# Get user input
+name = input("What is your name? ")
+
+# Greet the user
+print(f"Nice to meet you, {name}!")
